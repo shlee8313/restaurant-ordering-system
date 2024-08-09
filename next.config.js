898564b48907a17f615d87f61c53cb62f773a2c2
shlee <lsh.makem@gmail.com>
2024-08-09 09:32:43 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
   output: "export",
   images: {
     domains: ["via.placeholder.com"],
